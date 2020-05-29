7346550b91b5db7aeccf8794c8c78bdb7b189ec2
Nam Tran <nhtranngoc@wpi.edu>
2020-05-28 21:18:21 -0400

    Travis pls, fixed install_cpputest.sh

--- install_cpputest.sh
+++ install_cpputest.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-cd $HOME
+cd ${HOME}
 git clone git://github.com/cpputest/cpputest.git
 cd cpputest/build
 autoreconf .. -i
 ../configure
 make
 
-export CPPUTEST_HOME=$HOME/cpputest 
+export CPPUTEST_HOME=${HOME}/cpputest
+echo ${CPPUTEST_HOME}
