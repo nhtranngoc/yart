d26ce05e2f87c8d6b3ea888b42f340f930e902e4
Nam Tran <nhtranngoc@wpi.edu>
2020-05-28 20:43:59 -0400

    Modified install_cpputest.sh, fixed typo

--- install_cpputest.sh
+++ install_cpputest.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+cd $HOME
 git clone git://github.com/cpputest/cpputest.git
 cd cpputest/build
 autoreconf .. -i
 ../configure
 make
 
-export CPPUTEST_HOME=./cpputest 
+export CPPUTEST_HOME=$HOME/cpputest 
