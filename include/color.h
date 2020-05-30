08998bb2e3f5a68c1d61a65ba1c358bde298125e
Nam Tran <nhtranngoc@wpi.edu>
2020-05-30 10:15:24 -0400

    Added simple color operations & tests

--- include/color.h
+++ include/color.h
@@ -4,7 +4,14 @@
 #include "tuple.h"
 
 class Color : public Tuple {
+    public:
+        Color(float r, float g, float b) : Tuple(r,g,b,0){}
 
+        inline float r() {return this->x;}
+        inline float g() {return this->y;}
+        inline float b() {return this->z;}
+        inline uint32_t toHex() {return (0xff000000 | (((uint8_t) (this->x*255)) << 16) | (((uint8_t) (this->y*255)) << 8) | ((uint8_t) (this->z*255)));}
+        friend inline Color operator * (Color const &c1, Color const c2) {return Color(c1.x * c2.x, c1.y * c2.y, c1.z * c2.z);}
 };
 
 #endif // COLOR_H_
--- tests/colorTester.cpp
+++ tests/colorTester.cpp
@@ -10,3 +10,44 @@ TEST_GROUP(ColorTest) {
 
     }
 };
+
+TEST(ColorTest, AddingColors) {
+    Color c1(0.9, 0.6, 0.75);
+    Color c2(0.7, 0.1, 0.25);
+
+    CHECK((c1 + c2) == Color(1.6, 0.7, 1.0));
+}
+
+TEST(ColorTest, SubtractingColors) {
+    Color c1(0.9, 0.6, 0.75);
+    Color c2(0.7, 0.1, 0.25);
+
+    CHECK((c1 - c2) == Color(0.2, 0.5, 0.5));
+}
+
+TEST(ColorTest, MultiplyingColorByAScalar) {
+    Color c(0.2, 0.3, 0.4);
+
+    CHECK((c * 2) == Color(0.4, 0.6, 0.8));
+}
+
+TEST(ColorTest, MultiplyingColors) {
+    Color c1(1, 0.2, 0.4);
+    Color c2(0.9, 1, 0.1);
+
+    CHECK((c1 * c2) == Color(0.9, 0.2, 0.04));
+}
+
+TEST(ColorTest, ConvertToHex) {
+    Color c1(0,0,0);
+    LONGS_EQUAL(0xff000000, c1.toHex());
+    Color c2(1,0,0);
+    LONGS_EQUAL(0xffff0000, c2.toHex());
+    Color c3(0,1,0);
+    LONGS_EQUAL(0xff00ff00, c3.toHex());
+    Color c4(0,0,1);
+    LONGS_EQUAL(0xff0000ff, c4.toHex());
+
+    Color c5(0.5,0.5,0.5);
+    LONGS_EQUAL(0xff7f7f7f, c5.toHex());
+}
